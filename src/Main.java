e635a29080320c71978f2bcdbf007995eafd48c8
Mike88870687-cmyk <m88870687@gmail.com>
2026-02-03 21:00:14 +0200

    Refactor main logic into separate methods

--- src/Main.java
+++ src/Main.java
@@ -7,28 +7,19 @@ public class Main {
         String[] tasks = new String[100];
         int taskCount = 0;
         while (true) {
-            System.out.println("===TASK MANAGER===");
-            System.out.println("1 - Add task");
-            System.out.println("2 - Show task");
-            System.out.println("3 - Delete task");
-            System.out.println("0 - Exit");
-
+            printMenu();
             int choice = scanner.nextInt();
             scanner.nextLine();
 
             switch (choice){
                 case 1:
-                    System.out.println("Please add a task");
-                    String taskText = scanner.nextLine();
-                    tasks[taskCount] = taskText;
-                    taskCount++;
-                    System.out.println("Task added");
+                   taskCount = addTask(scanner, tasks, taskCount);
                     break;
                 case 2:
-                    System.out.println("Show task");
+                   showTasks(tasks, taskCount);
                     break;
                 case 3:
-                    System.out.println("Delete task");
+                    taskCount = deleteTask(scanner, tasks, taskCount);
                     break;
                 case 0:
                     System.out.println("Bye");
@@ -37,5 +28,60 @@ public class Main {
                     System.out.println("Invalid option");
             }
         }
+    }
+
+    static void printMenu(){
+        System.out.println("===TASK MANAGER===");
+        System.out.println("1 - Add task");
+        System.out.println("2 - Show task");
+        System.out.println("3 - Delete task");
+        System.out.println("0 - Exit");
+    }
+
+    static int addTask(Scanner scanner, String[] tasks, int taskCount){
+        System.out.println("Please add a task");
+        String taskText = scanner.nextLine();
+        tasks[taskCount] = taskText;
+        taskCount++;
+        System.out.println("Task added");
+        return taskCount;
+    }
+
+    static void showTasks(String[] tasks, int taskCount){
+        if(taskCount == 0) {
+            System.out.println("No task");
+        }else{
+            for(int i = 0; i < taskCount; i++){
+                System.out.println((i+1) + ". " + tasks[i]);
+            }
+        }
+    }
+
+    static int deleteTask(Scanner scanner, String[] tasks, int taskCount){
+        if(taskCount == 0) {
+            System.out.println("No task to delete");
+        }
+        for(int i = 0; i < taskCount; i++){
+            System.out.println((i+1) + ". " + tasks[i]);
+        }
+
+        System.out.println("Enter task number to delete");
+        int number = scanner.nextInt();
+        scanner.nextLine();
+
+        int index = number-1;
+
+        if(index<0||index>=taskCount){
+            System.out.println("Invalid task number");
+        }
+        for(int i = index; i<taskCount -1; i++){
+            tasks[i] = tasks[i+1];
+        }
+
+        tasks[taskCount -1] = null;
+        taskCount --;
+
+        System.out.println("Task deleted");
+        return taskCount;
     }
 }
